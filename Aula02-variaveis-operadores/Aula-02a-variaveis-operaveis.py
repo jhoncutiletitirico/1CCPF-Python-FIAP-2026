3227f589a82c35f580f6b326900617ccb17137f7
jhoncutiletitirico <jhoncutilegames@gmail.com>
2026-03-17 11:41:13 -0300

    Aula 02

---
+++ Aula02-variaveis-operadores/Aula-02a-variaveis-operaveis.py
@@ -0,0 +1,28 @@
+print("Olá Mundo")
+# exibir no console um texto strings
+print(7 + 4)
+# exibir calculo de soma entre 2 numeros
+print("7" + "4")
+# exibir no console a junção de 2 textos strings
+
+'''
+    comentários de
+    multiplas
+    linhas
+'''
+
+# Variaveis
+nome = "Jhon" # str
+idade = 18    # int
+peso = 84.1   # float
+
+print(idade, idade, peso)
+print(f"Oiii, {nome}!")
+
+# Input -- Simulação de forms no cmd
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("digite o seu peso: "))
+
+print(f"Com base na suas informações, seu nome é {nome}, você tem {idade} anos e pesa {peso}kg")
---
+++ Aula02-variaveis-operadores/Desafio-01, aula02a.py
@@ -0,0 +1,17 @@
+nome = input("Digite seu nome: ")
+
+print(f"Seja bem vindo ao terminal Python {nome}, poderia me dizer a sua data de nascimento?")
+
+
+DiaNasc = int(input("Dia do Nascimento: "))
+MesNasc = int(input("Mês do Nascimento: "))
+AnoNasc = int(input("Ano do Nascimento: "))
+
+print(f"Certo, você nasceu no dia {DiaNasc} no mês {MesNasc} em {AnoNasc}")
+print(f"{DiaNasc}/{MesNasc}/{AnoNasc})
+
+print("Que tal fazermos um calculo super mega ultra matematico juntos? é so me dizer os valores para fazer a soma")
+n1 = int(input("me diga um valor: "))
+n2 = int(input("agora me diga outro valor: "))
+
+print(n1 + n2)
