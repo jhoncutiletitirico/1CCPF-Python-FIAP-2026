04927bab1a032b230f763fa5c2ce65a3d29a01c1
jhoncutiletitirico <jhoncutilegames@gmail.com>
2026-04-28 11:37:53 -0300

    a

---
+++ aula04-repeticoes/aula04b-for.py
@@ -0,0 +1,18 @@
+# for count_music in range(3):
+#     print(f"Música {count_music}")
+#
+# # de 1 até 11, pulando de 2 em 2
+# for i in range(1, 12, 2):
+#     print(i)
+
+# ATV. 3
+# qtd_musicas = int(input("Qtd de mscs (DB): "))
+#
+# for i in range(qtd_musicas):
+#     print(f"Música {i}")
+
+# LAÇOS ANINHADOS
+# REP. ENCADEADA
+for i in range(0, 4):
+    for j in range(0, 3):
+        print(f"Link:{i}, Titulo :{j}")
